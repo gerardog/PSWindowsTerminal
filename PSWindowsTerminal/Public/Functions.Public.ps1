f1172cd3e18c9556781e20d4fa7075d0df1a984c
Gerardo Grignoli <gerardog@gmail.com>
2022-10-01 08:46:11 -0300

    Added Get-ConsoleHostProcessId and Test-IsWindowsTerminal

--- PSWindowsTerminal/Public/Functions.Public.ps1
+++ PSWindowsTerminal/Public/Functions.Public.ps1
@@ -20,3 +20,32 @@ Function Set-WTBackgroundImage {
 
     $Config | ConvertTo-Json -Depth 99 | Out-File -FilePath $ProfilePath -Force -Encoding utf8
 }
+
+function Get-ConsoleHostProcessId {
+  
+    # Save Original ConsoleHost title	
+    $oldTitle=$host.ui.RawUI.WindowTitle; 
+    # Set unique console title.
+    $r=(New-Guid); 
+    $host.ui.RawUI.WindowTitle = "$r"; 
+    #Find console window by title, then find console PID.
+    $result = (tasklist.exe /FO LIST /FI "WINDOWTITLE eq $r") | Select-String -Pattern  "PID:*" -Raw
+    
+    if ($null -ne $result) {
+        $consolePid = [int]($result.SubString(5).Trim());
+    } else {
+        $consolePid = 0;
+    }        
+    # Restore original ConsoleHost title.
+    $host.ui.RawUI.WindowTitle=$oldTitle;
+
+    return [int]$consolePid;
+}
+  
+function Test-IsWindowsTerminal {
+    $consolePid = Get-ConsoleHostProcessId;
+    if ($consolePid -gt 0) {
+        return (Get-Process -PID (Get-ConsoleHostProcessId)).ProcessName -eq "WindowsTerminal";
+    }
+    return $false;
+}
